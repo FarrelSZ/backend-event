abc89938054221f43f049572a53412528272e640
FarrelSZ <farel.it12@gmail.com>
2025-08-12 23:18:59 +0700

    Refactor response error and feat: API Location

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import mongoose from "mongoose";
 import * as Yup from "yup";
 
 type Pagination = {
@@ -18,15 +19,47 @@ export default {
     });
   },
   error(res: Response, error: unknown, message: string) {
+    // error untuk yup
     if (error instanceof Yup.ValidationError) {
-      res.status(500).json({
+      res.status(400).json({
         meta: {
           status: 400,
           message,
         },
-        data: error.errors,
+        data: {
+          [`${error.path}`]: error.errors[0],
+        },
       });
     }
+    // error mongoose
+    if (error instanceof mongoose.Error) {
+      res.status(500).json({
+        meta: {
+          status: 500,
+          message: error.message,
+        },
+        data: error.name,
+      });
+    }
+    // error dari data nya langsung
+    if ((error as any)?.code) {
+      const _err = error as any;
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: _err.errorResponse.errmsg,
+        },
+        data: _err,
+      });
+    }
+
+    res.status(500).json({
+      meta: {
+        status: 500,
+        message: message,
+      },
+      data: error,
+    });
   },
   unauthorized(res: Response, message: string = "unauthorized") {
     res.status(403).json({
